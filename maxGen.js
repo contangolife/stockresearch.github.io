9e9e98b662bbbb7763ecab001eafcb8044f9d8de
seokjoo <107666868+contangolife@users.noreply.github.com>
2025-06-30 02:14:46 +0900

    .

--- maxGen.js
+++ maxGen.js
@@ -3,7 +3,7 @@ const maxGen = 25;
 // 만약 id가 generation-list인 ul이 있으면 거기에 버튼 자동 생성
 const genList = document.getElementById('generation-list');
 if (genList) {
-  for (let i = 1; i <= maxGen; i++) {
+  for (let i = maxGen; i >= 1; i--) {   // ← 내림차순
     const li = document.createElement('li');
     const a = document.createElement('a');
     a.className = 'gen-btn';
